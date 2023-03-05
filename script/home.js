fa0347cf7f6387223421328991f384f7fb10afd3
TGHuybu <21127307@student.hcmus.edu.vn>
2023-03-05 23:09:38 +0700

    test some stuff i guess
    i'm gonna add a cv template later :33

--- script/home.js
+++ script/home.js
@@ -10,6 +10,10 @@ window.addEventListener("scroll", function() {
     background.style.backgroundPositionY = - +window.pageYOffset/2 + "px";
 });
 
+// yea copied from the net, god i am dumb why am i even here
+// someone please help me i am so useless please do something
+// i can't take it anymore what on earth is a java what the hell is a script
+// what even is jquery
 $(function() {
     $('.to-projects').click(function() {
         $('html, body').animate({scrollTop: $('#project-wrapper').offset().top }, 'slow');
