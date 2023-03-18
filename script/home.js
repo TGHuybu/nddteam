53860dc4d510f76315034a9491dc1b959f294f37
TGHuybu <21127307@student.hcmus.edu.vn>
2023-03-18 22:16:53 +0700

    home page UI adjustment
    i discovered keyframes!! omg

--- script/home.js
+++ script/home.js
@@ -1,14 +1,3 @@
-var background = document.getElementById("bg-img");
-// var home = document.getElementById("home");
-// var projects = document.getElementById("projects");
-
-// var height = screen.height;
-
-window.addEventListener("scroll", function() {
-    background.style.opacity = 1 - +window.pageYOffset/550 + "";
-    background.style.top = +window.pageYOffset + "px";
-    background.style.backgroundPositionY = - +window.pageYOffset/2 + "px";
-});
 
 // yea copied from the net, god i am dumb why am i even here
 // someone please help me i am so useless please do something
@@ -16,7 +5,7 @@ window.addEventListener("scroll", function() {
 // what even is jquery
 $(function() {
     $('.to-projects').click(function() {
-        $('html, body').animate({scrollTop: $('#project-wrapper').offset().top }, 'slow');
+        $('html, body').animate({scrollTop: $('#section-title').offset().top }, 'slow');
         return false;
     });
 });
