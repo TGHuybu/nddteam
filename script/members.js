be1f9c219cfb81318f57e9f57231d8a2305a9a68
TGHuybu <21127307@student.hcmus.edu.vn>
2023-09-20 16:59:34 +0700

    total reconstruction

--- script/members.js
+++
@@ -1,7 +0,0 @@
-var background = document.getElementById("bg-img");
-
-window.addEventListener("scroll", function() {
-    background.style.opacity = 1 - +window.pageYOffset/550 + "";
-    background.style.top = +window.pageYOffset + "px";
-    background.style.backgroundPositionY = - +window.pageYOffset/2 + "px";
-});
