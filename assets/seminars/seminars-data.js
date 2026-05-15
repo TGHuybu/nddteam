c386950fc172089def72934e22bb9f1a69494236
TGHuybu <21127307@student.hcmus.edu.vn>
2026-05-15 23:40:09 +0700

    add seminar tab template

---
+++ assets/seminars/seminars-data.js
@@ -0,0 +1,48 @@
+// Seminars data - organized by seminar ID
+const seminarsData = {
+  'seminar_1': {
+    "title": "Title Template",
+    "speaker_name": "Dr. Speaker Name",
+    "speaker_affiliation": "Affiliation Template",
+    "date": "2026-05-22",
+    "time": "2:00 PM",
+    "venue": "Virtual (Zoom)",
+    "abstract": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+    Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi \
+    ut aliquip ex ea commodo consequat.",
+    "external_links": [
+      { "label": "Zoom Link", "url": "example.com" }
+    ]
+  },
+  'seminar_2': {
+    "title": "Title Template",
+    "speaker_name": "Dr. Speaker Name",
+    "speaker_affiliation": "Affiliation Template",
+    "date": "2026-05-01",
+    "time": "2:00 PM",
+    "venue": "Virtual (Zoom)",
+    "abstract": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+    Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi \
+    ut aliquip ex ea commodo consequat.",
+    "external_links": [
+      { "label": "Zoom Link", "url": "example.com" }
+    ]
+  },
+  'seminar_3': {
+    "title": "Title Template",
+    "speaker_name": "Dr. Speaker Name",
+    "speaker_affiliation": "Affiliation Template",
+    "date": "2026-04-22",
+    "time": "2:00 PM",
+    "venue": "Virtual (Zoom)",
+    "abstract": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+    Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi \
+    ut aliquip ex ea commodo consequat.",
+    "external_links": [
+      { "label": "Zoom Link", "url": "example.com" }
+    ]
+  },
+};
