ec5080b0f322319c19a0a29a1a71eba0c77e58e3
TGHuybu <21127307@student.hcmus.edu.vn>
2026-06-04 19:50:42 +0700

    update seminar template

--- assets/seminars/seminars-data.js
+++ assets/seminars/seminars-data.js
@@ -1,46 +1,25 @@
 // Seminars data - organized by seminar ID
 const seminarsData = {
   'seminar_1': {
-    "title": "Title Template",
-    "speaker_name": "Dr. Speaker Name",
-    "speaker_affiliation": "Affiliation Template",
+    "title": "[Seminar Title Template]",
+    "speaker_name": "[Speaker Name Template]",
+    "speaker_affiliation": "[Affiliation Template]",
     "date": "2026-05-22",
     "time": "2:00 PM",
     "venue": "Virtual (Zoom)",
-    "abstract": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
-    Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \
-    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi \
-    ut aliquip ex ea commodo consequat.",
+    "abstract": "[Under construction]",
     "external_links": [
       { "label": "Zoom Link", "url": "example.com" }
     ]
   },
   'seminar_2': {
-    "title": "Title Template",
-    "speaker_name": "Dr. Speaker Name",
-    "speaker_affiliation": "Affiliation Template",
-    "date": "2026-05-01",
+    "title": "[Seminar Title Template]",
+    "speaker_name": "[Speaker Name Template]",
+    "speaker_affiliation": "[Affiliation Template]",
+    "date": "2026-08-20",
     "time": "2:00 PM",
     "venue": "Virtual (Zoom)",
-    "abstract": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
-    Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \
-    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi \
-    ut aliquip ex ea commodo consequat.",
-    "external_links": [
-      { "label": "Zoom Link", "url": "example.com" }
-    ]
-  },
-  'seminar_3': {
-    "title": "Title Template",
-    "speaker_name": "Dr. Speaker Name",
-    "speaker_affiliation": "Affiliation Template",
-    "date": "2026-04-22",
-    "time": "2:00 PM",
-    "venue": "Virtual (Zoom)",
-    "abstract": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
-    Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \
-    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi \
-    ut aliquip ex ea commodo consequat.",
+    "abstract": "[Under construction]",
     "external_links": [
       { "label": "Zoom Link", "url": "example.com" }
     ]
